b747562c4fc40319aa8ff8c481ca3626e9099b19
vnunez7 <vnunez7@illinois.edu>
2023-04-11 19:59:30 -0500

    Initial Commit

---
+++ MusicLova/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicLova
+//
+//  Created by Victoria Nunez on 4/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
